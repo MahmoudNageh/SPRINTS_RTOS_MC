dfbd4f504813ee70f194f4be5dd102866083011c
MahmoudNageh <90114735+MahmoudNageh@users.noreply.github.com>
2022-05-19 23:52:59 +0400

    Update Task1_1.c

--- Task1_1.c
+++ Task1_1.c
@@ -1,47 +1,43 @@
+/* Task 1: Use an existing RTOS project to write a program with one task to toggle an LED every 1000ms. */
+
+
+/* The Task Handler */
 TaskHandle_t ToggleTaskHandler = NULL;
 
-/*
- * Configure the processor for use with the Keil demo board.  This is very
- * minimal as most of the setup is managed by the settings in the project
- * file.
- */
+
 static void prvSetupHardware( void );
 /*-----------------------------------------------------------*/
 
-
+/* The task to be created */
 void Toggle_Task( void * pvParameters )
 {
     for( ;; )
     {
-			GPIO_write(PORT_0, PIN0, PIN_IS_HIGH);
-			
-			vTaskDelay(1000);
-				
-			GPIO_write(PORT_0, PIN0, PIN_IS_LOW);
-			
-			vTaskDelay(1000);
+	    GPIO_write(PORT_0, PIN0, PIN_IS_HIGH); /* Set Pin 0 to High */
+	    
+	    vTaskDelay(1000);                      /* Wait for 1000ms = 1s */
+	    
+	    GPIO_write(PORT_0, PIN0, PIN_IS_LOW); /* Set Pin 0 to Low */
+	    
+	    vTaskDelay(1000);                     /* Wait for 1000ms = 1s */
     }
 }
 
 
-/*
- * Application entry point:
- * Starts all the other tasks, then starts the scheduler. 
- */
 int main( void )
 {
 	/* Setup the hardware for use with the Keil demo board. */
 	prvSetupHardware();
 
-    /* Create Tasks here */
+    /* Create Tasks here, Storing the handle */
 
 	xTaskCreate(
-								Toggle_Task,          /* Function that implements the task. */
-                "Toggle Task",        /* Text name for the task. */
-                100,                  /* Stack size in words, not bytes. */
-                ( void * ) 0,         /* Parameter passed into the task. */
-                1,                    /* Priority at which the task is created. */
-                &ToggleTaskHandler ); /* Used to pass out the created task's handle. */
+			Toggle_Task,          /* Function that implements the task. */
+			"Toggle Task",        /* Text name for the task. */
+			100,                  /* Stack size in words, not bytes. */
+			( void * ) 0,         /* Parameter passed into the task. */
+			1,                    /* Priority at which the task is created. */
+			&ToggleTaskHandler ); /* Used to pass out the created task's handle. */
 
 	/* Now all the tasks have been started - start the scheduler.
 
