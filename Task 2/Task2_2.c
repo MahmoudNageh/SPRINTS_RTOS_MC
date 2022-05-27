2e987d7401eead341b0a9eafbce4a530c435ba81
MahmoudNageh <90114735+MahmoudNageh@users.noreply.github.com>
2022-05-27 21:58:49 +0400

    Update Task2_2.c

--- Task 2/Task2_2.c
+++ Task 2/Task2_2.c
@@ -1,171 +1,100 @@
-/*
- * FreeRTOS Kernel V10.2.0
- * Copyright (C) 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * http://www.FreeRTOS.org
- * http://aws.amazon.com/freertos
- *
- * 1 tab == 4 spaces!
+/* Task 2: Use an existing RTOS project to write a program with 2 tasks.
+ * Both tasks are responsible for writing on UART any string 10 times each
+ * task cycle. The task cycles are, respectively, 100ms, and 500ms.
+ * The task with cycle 500ms shall have an empty loop that loops 100,000 times
+ * (To simulate heavy load) after every time a string is written on UART.
+ * Each task writing to UART shall not be interrupted by the other task.
  */
 
-/* 
-	NOTE : Tasks run in system mode and the scheduler runs in Supervisor mode.
-	The processor MUST be in supervisor mode when vTaskStartScheduler is 
-	called.  The demo applications included in the FreeRTOS.org download switch
-	to supervisor mode prior to main being called.  If you are not using one of
-	these demo application projects then ensure Supervisor mode is used.
-*/
-
-
-/*
- * Creates all the demo application tasks, then starts the scheduler.  The WEB
- * documentation provides more details of the demo application tasks.
- * 
- * Main.c also creates a task called "Check".  This only executes every three 
- * seconds but has the highest priority so is guaranteed to get processor time.  
- * Its main function is to check that all the other tasks are still operational.
- * Each task (other than the "flash" tasks) maintains a unique count that is 
- * incremented each time the task successfully completes its function.  Should 
- * any error occur within such a task the count is permanently halted.  The 
- * check task inspects the count of each task to ensure it has changed since
- * the last time the check task executed.  If all the count variables have 
- * changed all the tasks are still executing error free, and the check task
- * toggles the onboard LED.  Should any task contain an error at any time 
- * the LED toggle rate will change from 3 seconds to 500ms.
- *
+/* #include "semph.h"  in main.c 
+ * configSUPPORT_DYNAMIC_ALLOCATION set to 1 in FreeRTOSConfig.h
+ * configUSE_MUTEXES set to 1 in FreeRTOSConfig.h
+ * configUSE_PREEMPTION set to 1 in FreeRTOSConfig.h
  */
-
-/* Standard includes. */
-#include <stdlib.h>
-#include <stdio.h>
-
-/* Scheduler includes. */
-#include "FreeRTOS.h"
-#include "task.h"
-#include "lpc21xx.h"
-
-/* Peripheral includes. */
-#include "serial.h"
-#include "GPIO.h"
-
 
 /*-----------------------------------------------------------*/
 
-/* Constants to setup I/O and processor. */
-#define mainBUS_CLK_FULL	( ( unsigned char ) 0x01 )
-
-/* Constants for the ComTest demo application tasks. */
-#define mainCOM_TEST_BAUD_RATE	( ( unsigned long ) 115200 )
 
 /* Tasks Handlers */
 TaskHandle_t String1TaskHandler       = NULL;
 TaskHandle_t String2TaskHandler       = NULL;
 
-SemaphoreHandle_t xSemaphore = NULL;
-
-/*
- * Configure the processor for use with the Keil demo board.  This is very
- * minimal as most of the setup is managed by the settings in the project
- * file.
- */
-static void prvSetupHardware( void );
-/*-----------------------------------------------------------*/
+/*Mutex Handler */
+SemaphoreHandle_t UART_Mutex          = NULL;
 
 
 
-/* One for the Button state for the Button task
- * and the other is for the Pressed time on the button 
- */
+/* 2 Strings for each task */
 
-signed portCHAR C1 = 'a';
-portCHAR C2 = 'b';
-uint8_t i;
-uint32_t j;
+// const signed portCHAR String_1[5] = {"HELLO"};
+// const signed portCHAR String_2[5] = {"WORLD"};
 
+/* Used Chars for easier fault detection in UART */
+const signed portCHAR String_1 = 'A';
+const signed portCHAR String_2 = 'B';
 
-void vATask( void * pvParameters )
-{
-   /* Create a mutex type semaphore. */
-   xSemaphore = xSemaphoreCreateMutex();
-
-}
+/* to variables for the for loops*/  
+uint8_t i; 					/* i is a uint8 because it goes to only 10 */
+uint32_t j; 					/* j count to 100,000 so it's a uint32 */
 
 
-/* The button task to be created, Checks the button each 100ms */
+/* The String 1 task to be created, prints a string 10 times each 100ms */
 void String_1_Task( void * pvParameters )
 {
     for( ;; )
     {
-			if( xSemaphore != NULL )
-			{
-        /* See if we can obtain the semaphore.  If the semaphore is not
-        available wait 10 ticks to see if it becomes free. */
-        if( xSemaphoreTake( xSemaphore, 0 ) == pdTRUE )
-        {
-            /* We were able to obtain the semaphore and can now access the
-            shared resource. */
-
-            for (i=0; i < 10; i++)
-						{
-						xSerialPutChar(C1);
-						}
-
-            /* We have finished accessing the shared resource.  Release the
-            semaphore. */
-            xSemaphoreGive( xSemaphore );
-        }
-			}
-			
-			vTaskDelay(100);
+	    if( UART_Mutex != NULL )
+	    {
+		    if( xSemaphoreTake( UART_Mutex, 0 ) == pdTRUE )     /* See if we can obtain the Mutex. */
+		    {
+           		/* We were able to obtain the Mutex and can now access the shared resource. */
+			    
+			    for (i= 0; i < 10; i++)
+			    {
+				    
+				    xSerialPutChar (String_1);
+			    
+			    }
+			    
+			    xSemaphoreGive( UART_Mutex );  		 /* Release the Mutex. */
+		    }
+		    
+	    }
+	    
+	    vTaskDelay(100);
     }
+	
 }
 
+/* The String 2 task to be created, prints a string 10 times each 500ms with a heavy load */
 void String_2_Task( void * pvParameters )
 {
     for( ;; )
     {
-			if( xSemaphore != NULL )
-			{
-        /* See if we can obtain the semaphore.  If the semaphore is not
-        available wait 10 ticks to see if it becomes free. */
-        if( xSemaphoreTake( xSemaphore, 0 ) == pdTRUE )
-        {
-            /* We were able to obtain the semaphore and can now access the
-            shared resource. */
-
-            for (i= 0; i < 10; i++)
-						{
-							for (j = 0; j < 100000; j++)
-							{}
-					
-							xSerialPutChar(C2);
-						}	
-
-            /* We have finished accessing the shared resource.  Release the
-            semaphore. */
-            xSemaphoreGive( xSemaphore );
-        }
-			}
-			
-			vTaskDelay(500);
+	    if( UART_Mutex != NULL )
+	    {
+		    if( xSemaphoreTake( UART_Mutex, 0 ) == pdTRUE )    /* See if we can obtain the Mutex. */
+		    {
+			    /* We were able to obtain the Mutex and can now access the shared resource. */
+			    
+			    for (i= 0; i < 10; i++)
+			    {
+				    for (j = 0; j < 100000; j++)
+				    { }
+				    
+				    xSerialPutChar (String_2);
+				    
+			    }
+			    
+			    xSemaphoreGive( UART_Mutex );	/* Release the Mutex. */
+		    
+		    }
+	    
+	    }
+	    
+	    vTaskDelay(500);
     }
+	
 }
 
 
@@ -179,30 +108,31 @@ int main( void )
 	/* Setup the hardware for use with the Keil demo board. */
 	prvSetupHardware();
 
-    /* Create Tasks here */
+	/* Create a mutex type semaphore. */
+    	UART_Mutex = xSemaphoreCreateMutex( );
+	
+    	/* Create Tasks here */
 
 	xTaskCreate(
-								String_1_Task,          /* Function that implements the task. */
+		String_1_Task,          /* Function that implements the task. */
                 "String 1 Task",        /* Text name for the task. */
-                100,                  /* Stack size in words, not bytes. */
-                ( void * ) 0,         /* Parameter passed into the task. */
-                2,                    /* Priority at which the task is created. */
-                &String1TaskHandler ); /* Used to pass out the created task's handle. */
+                100,                    /* Stack size in words, not bytes. */
+                ( void * ) 0,           /* Parameter passed into the task. */
+                2,                      /* Priority at which the task is created. */        
+                &String1TaskHandler );  /* Used to pass out the created task's handle. */
 								
 
 	xTaskCreate(
-								String_2_Task,               /* Function that implements the task. */
+		String_2_Task,               /* Function that implements the task. */
                 "String 2 Task",             /* Text name for the task. */
-                100,                      /* Stack size in words, not bytes. */
-                ( void * ) 0,             /* Parameter passed into the task. */
-                1,                        /* Priority at which the task is created. */
+                100,                         /* Stack size in words, not bytes. */
+                ( void * ) 0,                /* Parameter passed into the task. */
+                1,                           /* Priority at which the task is created. */
                 &String2TaskHandler );      /* Used to pass out the created task's handle. */
-
 								
 
 
 	/* Now all the tasks have been started - start the scheduler.
-
 	NOTE : Tasks run in system mode and the scheduler runs in Supervisor mode.
 	The processor MUST be in supervisor mode when vTaskStartScheduler is 
 	called.  The demo applications included in the FreeRTOS.org download switch
@@ -231,5 +161,3 @@ static void prvSetupHardware( void )
 	VPBDIV = mainBUS_CLK_FULL;
 }
 /*-----------------------------------------------------------*/
-
-
