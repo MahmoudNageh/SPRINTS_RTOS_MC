1e1398326f3af6efaa063a3b90a1f56a4b822b5c
MahmoudNageh <90114735+MahmoudNageh@users.noreply.github.com>
2022-05-28 08:54:24 +0400

    Update Task2_1 w Sem.c

--- Task 2/Task2_1 w Sem.c
+++ Task 2/Task2_1 w Sem.c
@@ -1,103 +1,30 @@
-/*
- * FreeRTOS Kernel V10.2.0
- * Copyright (C) 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * http://www.FreeRTOS.org
- * http://aws.amazon.com/freertos
- *
- * 1 tab == 4 spaces!
+/* Task 1: Use an existing RTOS project to write a program with two tasks.
+ * A task to read a button state and a task to turn an LED ON/OFF.
+ * The LED shall toggle its state between ON/OFF with every press on the button.
+ * The action of toggling the LED shall take place after the user presses the button once and releases it.
  */
 
-/* 
-	NOTE : Tasks run in system mode and the scheduler runs in Supervisor mode.
-	The processor MUST be in supervisor mode when vTaskStartScheduler is 
-	called.  The demo applications included in the FreeRTOS.org download switch
-	to supervisor mode prior to main being called.  If you are not using one of
-	these demo application projects then ensure Supervisor mode is used.
-*/
-
-
-/*
- * Creates all the demo application tasks, then starts the scheduler.  The WEB
- * documentation provides more details of the demo application tasks.
- * 
- * Main.c also creates a task called "Check".  This only executes every three 
- * seconds but has the highest priority so is guaranteed to get processor time.  
- * Its main function is to check that all the other tasks are still operational.
- * Each task (other than the "flash" tasks) maintains a unique count that is 
- * incremented each time the task successfully completes its function.  Should 
- * any error occur within such a task the count is permanently halted.  The 
- * check task inspects the count of each task to ensure it has changed since
- * the last time the check task executed.  If all the count variables have 
- * changed all the tasks are still executing error free, and the check task
- * toggles the onboard LED.  Should any task contain an error at any time 
- * the LED toggle rate will change from 3 seconds to 500ms.
- *
+/* include "semphr.h" in main.c
+ * PIN0 in PORT0 set of INPUT in GPIO_cfg.c
+ * configSUPPORT_DYNAMIC_ALLOCATION set to 1 in FreeRTOSConfig.h
+ * configUSE_PREEMPTION set to 0 in FreeRTOSConfig.h
  */
-
-/* Standard includes. */
-#include <stdlib.h>
-#include <stdio.h>
-
-/* Scheduler includes. */
-#include "FreeRTOS.h"
-#include "task.h"
-#include "lpc21xx.h"
-#include "semphr.h"
-
-/* Peripheral includes. */
-#include "serial.h"
-#include "GPIO.h"
 
 
 /*-----------------------------------------------------------*/
 
-/* Constants to setup I/O and processor. */
-#define mainBUS_CLK_FULL	( ( unsigned char ) 0x01 )
-
-/* Constants for the ComTest demo application tasks. */
-#define mainCOM_TEST_BAUD_RATE	( ( unsigned long ) 115200 )
 
 /* Tasks Handlers */
 TaskHandle_t ButtonTaskHandler       = NULL;
-TaskHandle_t ToggleTaskHandler       = NULL;
+TaskHandle_t LEDTaskHandler       = NULL;
 
-SemaphoreHandle_t BinSem;
-
-
-/*
- * Configure the processor for use with the Keil demo board.  This is very
- * minimal as most of the setup is managed by the settings in the project
- * file.
- */
-static void prvSetupHardware( void );
-/*-----------------------------------------------------------*/
+/* Semaphore Handler */
+SemaphoreHandle_t LedSem;
 
 
-
-/* One for the Button state for the Button task
- * and the other is for the Pressed time on the button 
- */
-pinState_t ButtonState;
-pinState_t LEDState;
-pinState_t ToggleState;
+pinState_t ButtonState;				/* for the button state pressed or not */ 
+pinState_t LEDState;				/* for the current LED state ON or OFF */
+pinState_t ToggleState;				/* for the toggling, should take action or not */
 
 /* The button task to be created, Checks the button each 100ms */
 void Button_Task( void * pvParameters )
@@ -105,27 +32,27 @@ void Button_Task( void * pvParameters )
     for( ;; )
     {
 	    ButtonState = GPIO_read(PORT_0, PIN0);
+	    
+	    xSemaphoreTake( LedSem , ( TickType_t ) 0 );                                  /* Obtain the Semaphore  */
 			
-			xSemaphoreTake( BinSem, ( TickType_t ) 0 );
-			
-	    if (ButtonState == PIN_IS_HIGH)                                                /* If the button is pressed let the toggle state to be TRUE */
+	    if (ButtonState == PIN_IS_HIGH)                                               /* If the button is pressed let the toggle state to be TRUE */
 	    {
-				
-				ToggleState = pdTRUE;
+		    
+		    ToggleState = pdTRUE;
 	    
 	    }
-			
-			else if (  ButtonState == PIN_IS_LOW && ToggleState == pdTRUE )                 /* Take action after the button is released*/
-			{
-				
-				xSemaphoreGive( BinSem );
-				
-				ToggleState = pdFALSE;
-				
-			}
 	    
-			vTaskDelay(100);
+	    else if (  ButtonState == PIN_IS_LOW && ToggleState == pdTRUE )               /* Take action after the button is released AND the toggle state should be TRUE*/
+	    {
+		    
+		    xSemaphoreGive( LedSem );                                             /* Give the Semaphore to the LED task to do the toggling */
+		    
+		    ToggleState = pdFALSE;                                                /* Reset the toggle state */
+	    }
+	    
+	    vTaskDelay(100);
     }
+	
 }
 
 /* The LED task to be created */
@@ -133,13 +60,13 @@ void LED_Task( void * pvParameters )
 {
     for( ;; )
     {
-	    if (xSemaphoreTake( BinSem, ( TickType_t ) 0 ) == pdTRUE)                          /*  */
+	    if (xSemaphoreTake( BinSem, ( TickType_t ) 0 ) == pdTRUE)                       /* See if we can obtain the Semaphore */
 	    {
-			    LEDState = GPIO_read(PORT_0, PIN1);                                            /* Read the current state of the LED ON or OFF */
-			    
-			    GPIO_write(PORT_0, PIN1, !LEDState);                                           /* Toggle the LED State */
-			    
-			    xSemaphoreGive( BinSem );
+		    LEDState = GPIO_read(PORT_0, PIN1);                                     /* Read the current state of the LED ON or OFF */
+		    
+		    GPIO_write(PORT_0, PIN1, !LEDState);                                    /* Toggle the LED State */
+		    
+		    xSemaphoreGive( LedSem );                                               /* Release the Semaphore */
 		    
 	    }
 	    
@@ -158,13 +85,14 @@ int main( void )
 	/* Setup the hardware for use with the Keil demo board. */
 	prvSetupHardware();
 	
-		BinSem = xSemaphoreCreateBinary();
+	/* Create a binary type semaphore. */
+	LedSem = xSemaphoreCreateBinary();
 		
 		
-    /* Create Tasks here */
+    	/* Create Tasks here */
 
 	xTaskCreate(
-								Button_Task,          /* Function that implements the task. */
+		Button_Task,          /* Function that implements the task. */
                 "Button Task",        /* Text name for the task. */
                 100,                  /* Stack size in words, not bytes. */
                 ( void * ) 0,         /* Parameter passed into the task. */
@@ -173,12 +101,12 @@ int main( void )
 								
 								
 	xTaskCreate(
-								LED_Task,          /* Function that implements the task. */
-                "Toggle Task",        /* Text name for the task. */
+		LED_Task,          /* Function that implements the task. */
+                "LED Task",        /* Text name for the task. */
                 100,                  /* Stack size in words, not bytes. */
                 ( void * ) 0,         /* Parameter passed into the task. */
                 1,                    /* Priority at which the task is created. */
-                &ToggleTaskHandler ); /* Used to pass out the created task's handle. */
+                &LEDTaskHandler ); /* Used to pass out the created task's handle. */
 
 	/* Now all the tasks have been started - start the scheduler.
 
